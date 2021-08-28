497dd0b56440f17242e3d576e41ae4b2083cd150
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-08-28 22:13:40 +0600

    Full Website Done

--- js/weather.js
+++ js/weather.js
@@ -36,7 +36,7 @@ const displayData = data => {
         div.innerHTML = `
         <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="" >
         <h1>${data.name}</h1>
-        <h3><span>${data.main.temp}</span>&deg;C</h3>
+        <h3><span>${Math.round(data.main.temp - 273.15)}</span>&deg;C</h3>
         <h1 class="lead">${data.weather[0].main}</h1>
         `
         container.appendChild(div);
